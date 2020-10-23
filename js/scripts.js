c5fde9597fb4b1a28bfc69ea3d9658ae33818665
Patrick Osten <Patrickosten040@gmail.com>
2020-10-23 09:28:08 -0700

    attach button to form

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
+
+
 $(document).ready(function() {
-  $("#formOne").submit(function(event) {
-    event.preventDefault()
-    const value1 = parseInt($("1").val());
-    alert(value1)
+  $("#formOne").submit(function() {
+    const choice1 = $("option#1").val();
+    $("#python").append("Python!");
+  });
+});
