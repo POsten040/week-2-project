b8865fd50bc562ef7673b6bbc4948ad755cd9f8d
Patrick Osten <Patrickosten040@gmail.com>
2020-10-23 12:11:50 -0700

    make the submit funciton work

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
-const firstanswer = $("#first").val();
+const firstanswer = parseInt($("#first").val());
 
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     if (firstanswer > 0) {
       $("#python").append("Python!");
-    }
     event.preventDefault()
+    }
   });
 });
 
