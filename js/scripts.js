7d677f18fa6baacae30ea24412cbc841eac42075
Patrick Osten <Patrickosten040@gmail.com>
2020-10-23 12:41:59 -0700

    add 'else if's to js function, clean up code

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,16 @@
-const firstanswer = parseInt($("#first").val());
+
 
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    if (firstanswer > 0) {
+    const firstanswer = parseInt($("#first").val());
+    const secondanswer = parseInt($("#second").val());
+    const thirdanswer = parseInt($("#third").val());
+    const fourthanswer = parseInt($("#fourth").val());
+    const fifthanswer = parseInt($("#fifth").val());
+    const sum = firstanswer + secondanswer + thirdanswer + fourthanswer + fifthanswer
+    if (sum <= 5) {
       $("#python").append("Python!");
-    event.preventDefault()
+      event.preventDefault();
     }
   });
 });
